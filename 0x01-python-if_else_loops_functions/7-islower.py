a2ab69c1e9d480c207da5d836c6def52a5857587
benjamin ifejika <benjaminifejika393@gmail.com>
2022-12-04 05:42:27 -0800

    added new file

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def islower(c):
+    lowercase = list(range(97, 123))
+    uppercase = list(range(65, 91))
+    if ord(c) in lowercase:
+        return True
+    elise:
+        return False
