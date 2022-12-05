57e2baf39df0c1027e7482fdd54cf6b0ac81041c
benjamin ifejika <benjaminifejika393@gmail.com>
2022-12-05 10:16:47 -0800

    This program prints all integer in a  list, one per line

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    length = len(my_list)
-    list_index = length - 1
-    if idx < 0:
-        return my_list
-    elif idx > list_index:
-        return my_list
-    else:
+    if 0 <= idx < len(my_list):
         my_list[idx] = element
+    else:
+        return my_list
