2daa9e9c7df7497af6ccad5b064b759a0107e5ea
benjamin ifejika <benjaminifejika393@gmail.com>
2023-01-28 03:34:45 -0800

    function that writes a string to a text file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module provides a hands-on experience writing to a file"""
+
+
+def write_file(filename="", text=""):
+    """ This function writes to a file"""
+    if filename:
+        if text:
+            with open(filename, 'w', encoding='utf-8') as file_object:
+                return (file_object.write(text))
